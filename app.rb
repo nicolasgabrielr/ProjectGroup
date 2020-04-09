fbe0f94b36edf1d26a4346b382f7b85e51455085
Desktop <sArianV@github.com>
2020-04-09 19:27:52 -0300

    Estructura basica de la aplicacion

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 class App < Sinatra::Base
   get "/" do
-    "hola mundo cruel"
+    erb:home
+
+  end
+  get "/logeduser" do
+    erb:logeduser
+  end
+  get "/logedadmin" do
+    erb:logedadmin
+  end
+  get "/notificationlist" do
+    erb:notificationlist
+  end
+  get "/notificationdemo" do
+    erb:notificationdemo
+  end
+  get "/about" do
+    erb:about
+  end
+  get "/templatetest" do
+    erb:templatetest
   end
 end
