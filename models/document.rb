d5e1e420828bf314aca6b8f1ca1e114fed251742
Pablo_Ameri <poameri@gmail.com>
2020-05-26 01:30:10 -0300

    add queries in class documents

--- models/document.rb
+++ models/document.rb
@@ -5,7 +5,28 @@ class Document < Sequel::Model
     validates_presence [:resolution, :filename, :path, :realtime]
     validates_unique [:resolution, :filename, :path, :realtime]
   end
+  dataset_module do
+    def deleteds(true_or_false)
+      select(:filename, :resolution, :realtime).
+      where(deleted: true_or_false).
+      all
+    end
+    def by_resolution(resolution)
+      select(:filename, :resolution, :realtime).
+      where(resolution: resolution, deleted: false).
+      all
+    end
+    def by_date(initial_date, final_date)
+      select(:filename, :resolution, :realtime).
+      where(realtime: initial_date..final_date, deleted: false).
+      all
+    end
+    def by_date_and_user(initial_date, final_date, user)
+      select(:filename, :resolution, :realtime).
+      where(realtime: initial_date..final_date, user_id: user, deleted: false).
+      all
+    end
+  end
   many_to_many :users
   one_to_many :notifications
-  set_primary_key :id
 end
