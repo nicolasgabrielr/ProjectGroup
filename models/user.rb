812701dc03a1b2826edb9c8923cf6db619aa7ba3
PabloAmeri <poameri@gmail.com>
2020-10-05 23:46:11 -0300

    apply rubocop to models #38

--- models/user.rb
+++ models/user.rb
@@ -1,12 +1,11 @@
 class User < Sequel::Model
   plugin :validation_helpers
   def validate
-   super
-    validates_presence [:email, :name, :dni, :surname, :password, :username]
-    validates_unique [:email, :username, :dni]
-    validates_format /\A.*@.*\..*\z/, :email, message: 'is not a valid email'
-    validates_inclusion_of :dni, :in => 100000..999999999, message: 'is not a valid dni'
-
+    super
+    validates_presence %i[email name dni surname password username]
+    validates_unique %i[email username dni]
+    validates_format(/\A.*@.*\..*\z/, :email, message: 'is not a valid email')
+    validates_inclusion_of :dni, in: 100_000..999_999_999, message: 'is not a valid dni'
   end
   many_to_many :documents
   one_to_many :notifications
